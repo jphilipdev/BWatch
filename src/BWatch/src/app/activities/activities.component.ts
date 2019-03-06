73667361de455fd9d614a2b7b848c1137e1d273e
jonathan.philip@gmail.com <jonathan.philip@gmail.com>
2019-03-06 22:18:11 +1100

    Adding reactive form for activities

--- src/BWatch/src/app/activities/activities.component.ts
+++ src/BWatch/src/app/activities/activities.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ActivitiesService } from './activities.service';
 
 @Component({
   selector: 'activities',
-  templateUrl: './activities.html'
+  templateUrl: './activities.html',
+  styles: [`
+    .formError { background-color: red; }
+  `]
 })
 
-export class ActivitiesComponent {
+export class ActivitiesComponent implements OnInit {
 
   activities: any
+  activityForm: FormGroup
 
   constructor(private activitiesService: ActivitiesService) {
-    this.activities = activitiesService.getActivities();
+  }
+
+  ngOnInit() {
+    this.activities = this.activitiesService.getActivities();
+
+    let activityName = new FormControl('name', Validators.required);
+    this.activityForm = new FormGroup({
+      activityName
+    });
+  }
+
+  addActivity(formValues) {
+    if(this.activityForm.valid) {
+      console.log('activity', formValues.activityName)
+    }
   }
 }
--- src/BWatch/src/app/activities/activities.module.ts
+++ src/BWatch/src/app/activities/activities.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { activitiesRoutes } from './activities.routes';
 import { ActivitiesComponent } from './activities.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(activitiesRoutes)
   ],
   declarations: [ActivitiesComponent],
