b95e62e7ba20afcb753653cd6ace8a67d085b523
jonathan.philip@gmail.com <jonathan.philip@gmail.com>
2019-12-10 20:55:00 +1100

    Reinstating toastr for add food success/error messages

--- src/ClientApp/src/app/food/food.component.ts
+++ src/ClientApp/src/app/food/food.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AddFoodComponent } from './add-food.component';
 import { TOASTR_TOKEN, Toastr } from '@shared/services/toastr.service';
 import { loadFoods, addFood, addFoodSuccess } from '@actions/foodActions';
+import { tap, filter } from 'rxjs/operators';
 
 
 @Component({
@@ -15,7 +16,18 @@ import { loadFoods, addFood, addFoodSuccess } from '@actions/foodActions';
 
 export class FoodComponent implements OnInit {
   private foods$: Observable<any> = this.store.select(state => state.food.foods);
-  constructor(private store: Store<{food: any}>, public dialog: MatDialog, @Inject(TOASTR_TOKEN) private toastr: Toastr) {
+
+  constructor(private store: Store<{ food: any }>, public dialog: MatDialog, @Inject(TOASTR_TOKEN) private toastr: Toastr) {
+    this.showToastrSuccessWhenAddFoodSuccess();
+    this.showToastrErrorWhenAddFoodFailure();
+  }
+
+  private showToastrSuccessWhenAddFoodSuccess() {
+    this.store.pipe(select(state => state.food.addFood.success), filter(success => success)).subscribe(() => this.toastr.success('meh', 'Food Added!'));
+  }
+
+  private showToastrErrorWhenAddFoodFailure() {
+    this.store.pipe(select(state => state.food.addFood.failure), filter(failure => failure)).subscribe(() => this.toastr.error('meh', 'Error Adding Food'));
   }
 
   ngOnInit(): void {
@@ -23,7 +35,7 @@ export class FoodComponent implements OnInit {
   }
 
   private loadFoods() {
-    this.store.dispatch({type: loadFoods.type});
+    this.store.dispatch({ type: loadFoods.type });
   }
 
   onAddFoodClicked(): void {
@@ -32,15 +44,11 @@ export class FoodComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      this.addFood({name: result.foodName, quantity: result.foodQuantity});
+      this.addFood({ name: result.foodName, quantity: result.foodQuantity });
     })
   }
 
   private addFood(food): void {
-    this.store.dispatch({type: addFood.type, payload: food});
-    // this.foodService.addFood(food).subscribe(()=> {
-    //   this.toastr.success(food.name, 'Food Added!');
-    //   this.getFoods();
-    // });
+    this.store.dispatch({ type: addFood.type, payload: food });
   }
- }
+}
